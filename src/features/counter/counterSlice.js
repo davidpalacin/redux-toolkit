9dd55156019db0ffbddc4ed1e35d90f9f3f44d92
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2023-01-11 10:47:19 +0100

    create counter with redux toolkit

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,27 @@
+// Aquí ya se definen tanto las actions como los reducers
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
